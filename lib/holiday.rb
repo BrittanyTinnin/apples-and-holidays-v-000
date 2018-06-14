ca49da9ec576d80935fd29b064bacf41549d0b98
Brittany Tinnin <bntinnin7@gmail.com>
2018-06-14 17:49:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -85,5 +85,11 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  holiday_hash.each do |season, data|
+    season.each do |holiday, value|
+      if value == ["BBQ"]
+        puts holiday
+      end
+    end
+  end
 end
